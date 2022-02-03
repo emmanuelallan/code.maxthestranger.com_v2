13ba71781b7e5bbcab20bde9fc2a30aed592d9d6
maxthestranger <thestrangermax@gmail.com>
2022-02-03 20:47:37 +0300

    fix: changed the favicon to match the logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/images/logo.svg',
       },
     },
     'gatsby-plugin-mdx',
