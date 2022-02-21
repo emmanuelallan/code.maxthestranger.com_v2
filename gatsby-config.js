1b5826aa4747dd897f15febf2cb274f4edd9a214
maxthestranger <thestrangermax@gmail.com>
2022-02-21 16:42:36 +0300

    add new gtag for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,29 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.YOUR_GOOGLE_ANALYTICS_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        gtagConfig: {
+          optimize_id: "",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
       },
     },
     "gatsby-plugin-image",
